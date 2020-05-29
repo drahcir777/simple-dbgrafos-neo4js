73ed165ca21911fae7a455017938176dfbfdecc3
Richard <47856009+drahcir777@users.noreply.github.com>
2020-05-29 17:38:24 -0300

    Modifiquei pela segunda vez

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ const neo4j = require('neo4j-driver');
 const express = require('express')
 
 const app = express();
+// Conexão com o banco de dados neo4j
 var driver = neo4j.driver('bolt://54.146.71.112:33166', neo4j.auth.basic('neo4j', 'interval-complaint-craft'));
 
 
 
- /***** Essa query ela nos trás os resultados, bem parecido com o SQL *****/
-//var query = `MATCH (c1:Cidadao) WHERE c1.Nome = "Richard" RETURN c1`;
-
-  /*** Essa query ela nos trás os resultados dos paises */
-//var query = `MATCH (p1:Pais) WHERE p1.Nome = "Holanda" RETURN p1`;
-
 /**
+ * 
+ * 1º ETAPA
+ * 
  * Já essa query ela cria os registros dos Cidadãos e também os Paises, bata mudar as tags "c1" e os campos
  * dentro do {}
  * var query = `CREATE (c1:Cidadao {Nome: "Richard"}),
@@ -21,14 +19,36 @@ var driver = neo4j.driver('bolt://54.146.71.112:33166', neo4j.auth.basic('neo4j'
     (c4:Cidadao {Nome: "Rodrigo"})`;
  */
 
+ /*
+    2º ETAPA
+
+ var query = `CREATE (p1:Pais {Nome: "Holanda"}),
+(p2:Pais {Nome: "Brasil"}),
+(p3:Pais {Nome: "Noruega"}),
+(p4:Cidadao {Nome: "Suiça"})`;
+*/
 
 /**
+ *  3º ETAPA
+ */
+
+ /***** Essa query ela nos trás os resultados, bem parecido com o SQL *****/
+//var query = `MATCH (c1:Cidadao) WHERE c1.Nome = "Richard" RETURN c1`;
+
+  /*** Essa query ela nos trás os resultados dos paises */
+  // var query = `MATCH (p1:Pais) WHERE p1.Nome = "Holanda" RETURN p1`;
+
+
+/** 4º ETAPA
+ * 
  * Agora para criar um relacionamento entre as entidades basta usarmos essa query
  * var query = `CREATE (p1)-[re1:Morou]->(c1),
              (p1)-[r2:Viajou]->(c4)`;
  */
 
  /**
+  *  5º ETAPA
+  * 
   * Aqui buscamos os relacionamentos das duas entidades que foi criado.
   * var query = `MATCH(c1:Cidadao), (p1:Pais) return *`;
   */
@@ -41,7 +61,7 @@ var session = driver.session();
 //session.run(query, params)
 
 
-
+/** Esse codigo você só mantem quando for fazer um MATCH */
 session.run(query, params)
   .then(function(result) {
     result.records.forEach(function(record) {
@@ -51,6 +71,7 @@ session.run(query, params)
   .catch(function(error) {
     console.log(error);
   });
+
 
   
 
